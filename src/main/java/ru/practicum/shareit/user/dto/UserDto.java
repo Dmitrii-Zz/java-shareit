2462cf61f4ffca60d59bd6022752974a132d9c10
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-10-25 23:51:58 +0700

    Написать dto юзер

--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -1,2 +1,14 @@
-package ru.practicum.shareit.user.dto;public class UserDto {
+package ru.practicum.shareit.user.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private long id;
+    private String name;
+    private String email;
+
+    public UserDto(String name) {
+        this.name = name;
+    }
 }
