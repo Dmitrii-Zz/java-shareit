efab1ae9e2cf180238193d19843bba15c93a3edd
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-10-25 23:51:05 +0700

    Написать имплиментацию репозитория пользователя

--- src/main/java/ru/practicum/shareit/user/UserRepositoryImpl.java
+++ src/main/java/ru/practicum/shareit/user/UserRepositoryImpl.java
@@ -1,2 +1,40 @@
-package ru.practicum.shareit.user;public class UserRepositoryImpl {
+package ru.practicum.shareit.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+@Slf4j
+@RequiredArgsConstructor
+public class UserRepositoryImpl implements UserRepository {
+    private long id = 1;
+    private final Map<Long, User> users;
+
+    @Override
+    public User createUser(User user) {
+        user.setId(id);
+        users.put(id++, user);
+        return user;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return new ArrayList<>(users.values());
+    }
+
+    @Override
+    public User updateUser(User user) {
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public void deleteUser(long id) {
+        users.remove(id);
+    }
 }
