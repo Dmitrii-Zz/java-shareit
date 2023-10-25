0f6a196dcd32b0653cebc6c2bc483d51e0a247e9
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-10-25 23:50:30 +0700

    Написать интерфейс репозитория юзеров

--- src/main/java/ru/practicum/shareit/user/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/UserRepository.java
@@ -1,2 +1,15 @@
-package ru.practicum.shareit.user;public class UserRepository {
+package ru.practicum.shareit.user;
+
+import java.util.List;
+
+public interface UserRepository {
+
+    User createUser(User user);
+
+    List<User> getAllUsers();
+
+    User updateUser(User user);
+
+    void deleteUser(long id);
+
 }
