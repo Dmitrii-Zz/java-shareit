fa5f6fa1ecedb7e0a021eb20f96ff69f44c8d2a5
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-10-25 23:49:49 +0700

    Написать маппер пользователей

--- src/main/java/ru/practicum/shareit/user/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/UserMapper.java
@@ -1,2 +1,9 @@
-package ru.practicum.shareit.user;public class UserMapper {
+package ru.practicum.shareit.user;
+
+import ru.practicum.shareit.user.dto.UserDto;
+
+public class UserMapper {
+    public UserDto getUserDto(User user) {
+        return new UserDto(user.getName());
+    }
 }
