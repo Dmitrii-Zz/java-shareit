7a86ea520e4fe87362731b9f725427ade833a6c7
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-10-25 23:48:25 +0700

    Написать сущность пользователь

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,24 @@
 package ru.practicum.shareit.user;
 
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 /**
  * TODO Sprint add-controllers.
  */
+
+@Data
 public class User {
+    private long id;
+
+    @NotNull(message = "Отсутствует имя юзера.")
+    @NotBlank(message = "Имя не должно быть пустым.")
+    private String name;
+
+    @NotNull(message = "Отсутствует email.")
+    @Email(message = "Введен некорректный адрес электронной почты.")
+    private String email;
 }
