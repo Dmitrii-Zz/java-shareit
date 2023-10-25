cf218dfbb3e5f5b8bbd1f25c70652abba0e79ded
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-10-25 23:57:24 +0700

    Подготовить сущность вещи

--- src/main/java/ru/practicum/shareit/item/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/ItemMapper.java
@@ -1,2 +1,15 @@
-package ru.practicum.shareit.item;public class ItemMapper {
+package ru.practicum.shareit.item;
+
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Item;
+
+public class ItemMapper {
+    public static ItemDto toItemDto(Item item) {
+        return new ItemDto(
+                item.getName(),
+                item.getDescription(),
+                item.isAvailable(),
+                item.getRequest() != null ? item.getRequest().getId() : null
+        );
+    }
 }
--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,24 @@
 package ru.practicum.shareit.item.dto;
 
+import lombok.Data;
+import ru.practicum.shareit.user.User;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
 public class ItemDto {
+    private long id;
+    private String name;
+    private String description;
+    private boolean isAvailable;
+    private User owner;
+    private Long request;
+
+    public ItemDto(String name, String description, boolean isAvailable, Long request) {
+        this.name = name;
+        this.description = description;
+        this.isAvailable = isAvailable;
+        this.request = request;
+    }
 }
