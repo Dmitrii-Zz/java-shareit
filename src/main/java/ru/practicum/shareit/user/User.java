8bfcbbbd09b2a85586681ff312bc703bff0c1e48
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-10-28 21:04:56 +0700

    Убрать аннотацию NotNull

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -4,21 +4,15 @@ import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
-/**
- * TODO Sprint add-controllers.
- */
 
 @Data
 public class User {
     private long id;
 
-    @NotNull(message = "Отсутствует имя юзера.")
     @NotBlank(message = "Имя не должно быть пустым.")
     private String name;
 
-    @NotNull(message = "Отсутствует email.")
+    @NotBlank(message = "Отсутствует email.")
     @Email(message = "Введен некорректный адрес электронной почты.")
     private String email;
 }
