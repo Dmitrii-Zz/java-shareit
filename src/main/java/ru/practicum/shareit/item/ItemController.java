d5c80eaf0a82832ab7573401eadb020ac8a6113e
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-10-28 21:14:30 +0700

    Написать метод обнволения вещи

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.item.model.Item;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/items")
 @RequiredArgsConstructor
@@ -11,7 +13,15 @@ public class ItemController {
     private final ItemService itemService;
 
     @PostMapping
-    public Item createItem(@RequestHeader("X-Sharer-User-Id") long userId, @RequestBody Item item) {
+    public Item createItem(@RequestHeader("X-Sharer-User-Id") long userId,
+                           @Valid @RequestBody Item item) {
         return itemService.createItem(item, userId);
+    }
+
+    @PatchMapping("/{itemId}")
+    public Item updateItem(@RequestHeader("X-Sharer-User-Id") long userId,
+                           @PathVariable long itemId,
+                           @RequestBody Item item) {
+        return itemService.updateItem(item, userId, itemId);
     }
 }
