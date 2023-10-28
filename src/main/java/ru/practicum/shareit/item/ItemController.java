561bac58d5d474c09aee72c714804d5db5018bb7
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-10-28 23:35:30 +0700

    refactor: добавить логи в контроллер

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.item;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.item.model.Item;
 
@@ -10,12 +11,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/items")
 @RequiredArgsConstructor
+@Slf4j
 public class ItemController {
     private final ItemService itemService;
 
     @PostMapping
     public Item createItem(@RequestHeader("X-Sharer-User-Id") long userId,
                            @Valid @RequestBody Item item) {
+        log.info("Запрос создания вещи юзера id = " + userId);
         return itemService.createItem(item, userId);
     }
 
@@ -23,21 +26,25 @@ public class ItemController {
     public Item updateItem(@RequestHeader("X-Sharer-User-Id") long userId,
                            @PathVariable long itemId,
                            @RequestBody Item item) {
+        log.info("Обновление вещи пользователя id = " + userId);
         return itemService.updateItem(item, userId, itemId);
     }
 
     @GetMapping("/{itemId}")
     public Item getItemById(@PathVariable long itemId) {
+        log.info("Запрос вещи id = " + itemId);
         return itemService.getItemById(itemId);
     }
 
     @GetMapping
     public List<Item> getAllItemUser(@RequestHeader("X-Sharer-User-Id") long userId) {
+        log.info("Запрос всех вещей юзера id = " +userId);
         return itemService.findAllUsersItems(userId);
     }
 
     @GetMapping("/search")
     public List<Item> searchItem(@RequestParam String text) {
+        log.info("Поиск вещи по тексту = " + text);
         return itemService.searchItem(text);
     }
 }
--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,42 +1,46 @@
 package ru.practicum.shareit.user;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-/**
- * TODO Sprint add-controllers.
- */
 @RestController
 @RequestMapping(path = "/users")
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
     private final UserService userService;
 
     @PostMapping
     public User createUser(@Validated @RequestBody User user) {
+        log.info("Запрос на создание юзера.");
         return userService.createUser(user);
     }
 
     @GetMapping("/{userId}")
     public User getUserById(@PathVariable long userId) {
+        log.info("Запрос пользователя id = " + userId);
         return userService.getUserById(userId);
     }
 
     @GetMapping
     public List<User> getAllUsers() {
+        log.info("Запрос всех пользователей.");
         return userService.getAllUsers();
     }
 
     @PatchMapping("/{userId}")
     public User updateUser(@RequestBody User user, @PathVariable long userId) {
+        log.info("Обновление юзера id = " + userId);
         return userService.updateUser(user, userId);
     }
 
     @DeleteMapping("/{userId}")
     public void deleteUser(@PathVariable long userId) {
+        log.info("Удаление юзера id = " + userId);
         userService.deleteUser(userId);
     }
 }
