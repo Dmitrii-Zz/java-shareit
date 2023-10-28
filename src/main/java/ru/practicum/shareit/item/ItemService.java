c740addf48bc3f31e49092ca15b39ac9515809cd
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-10-28 21:12:44 +0700

    Написать метод обновления вещи

--- src/main/java/ru/practicum/shareit/item/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.item;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.practicum.shareit.exceptions.CheckUserException;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.UserService;
 
@@ -14,5 +15,28 @@ public class ItemService {
     public Item createItem(Item item, long userId) {
         item.setOwner(userService.getUserById(userId));
         return itemStorage.createItem(item);
+    }
+
+    public Item updateItem(Item item, long userId, long itemId) {
+        Item itemFromBd = itemStorage.getItemById(itemId);
+        boolean checkUser = itemFromBd.getOwner().getId() == userId;
+
+        if (!checkUser) {
+            throw new CheckUserException("Отсутствует доступ к вещи id = " + itemId);
+        }
+
+        if (item.getName() != null) {
+            itemFromBd.setName(item.getName());
+        }
+
+        if (item.getDescription() != null) {
+            itemFromBd.setDescription(item.getDescription());
+        }
+
+        if (item.getAvailable() != null) {
+            itemFromBd.setAvailable(item.getAvailable());
+        }
+
+        return itemStorage.update(itemFromBd);
     }
 }
