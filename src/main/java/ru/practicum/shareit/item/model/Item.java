4992cbd595ed1679a44ec7d2bb3b85121aea03dd
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-10-28 21:07:43 +0700

    Добавить валидацию полей сущности вещ

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -4,15 +4,25 @@ import lombok.Data;
 import ru.practicum.shareit.request.ItemRequest;
 import ru.practicum.shareit.user.User;
 
-/**
- * TODO Sprint add-controllers.
- */
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class Item {
+
     private long id;
+
+    @NotBlank(message = "В запросе отсутствует имя вещи.")
     private String name;
+
+    @NotBlank(message = "В запросе отсутствует описание вещи.")
     private String description;
-    private boolean available;
+
+    @NotNull(message = "В запросе отсутствует статус запроса к аренде.")
+    private Boolean available;
+
     private User owner;
+
     private ItemRequest request;
+
 }
