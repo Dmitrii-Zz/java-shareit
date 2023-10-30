4099bdfb23d6022c0d676318db242823bfa61d94
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-10-30 21:26:47 +0700

    refactor: добавить пакет repository для фичи item

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -6,6 +6,8 @@ import ru.practicum.shareit.item.*;
 import ru.practicum.shareit.item.controller.ItemController;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.mapper.ItemMapper;
+import ru.practicum.shareit.item.repository.ItemRepository;
+import ru.practicum.shareit.item.repository.ItemRepositoryImpl;
 import ru.practicum.shareit.user.controller.UserController;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.repository.UserRepository;
