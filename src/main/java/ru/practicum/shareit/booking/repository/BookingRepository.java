50ef3ea09bc87eb466427913cd4aae8f62d4de9e
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-11-08 22:16:22 +0700

    feat: написать интерфейс репозитория аренды

--- src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
@@ -1,2 +1,18 @@
-package ru.practicum.shareit.booking.repository;public class BookingRepository {
+package ru.practicum.shareit.booking.repository;
+
+import ru.practicum.shareit.booking.model.Booking;
+
+import java.util.List;
+
+public interface BookingRepository {
+
+    Booking createBooking(Booking booking);
+
+    Booking addStatusBooking(Booking booking);
+
+    Booking getBookingById(long bookingId);
+
+    List<Booking> findAllBookingByUserId(String state, long userId);
+
+    List<Booking> findAllBookingByOwnerId(String state, long userId);
 }
