f439af942a9ede3bb4191955915c10ce8be1af76
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-11-08 22:16:58 +0700

    feat: реализовать методы интерфейса репозитория аренды

--- src/main/java/ru/practicum/shareit/booking/repository/BookingRepositoryImpl.java
+++ src/main/java/ru/practicum/shareit/booking/repository/BookingRepositoryImpl.java
@@ -1,2 +1,35 @@
-package ru.practicum.shareit.booking.repository;public class BookingRepository {
+package ru.practicum.shareit.booking.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.practicum.shareit.booking.model.Booking;
+
+import java.util.List;
+
+@Repository
+public class BookingRepositoryImpl implements BookingRepository {
+
+    @Override
+    public Booking createBooking(Booking booking) {
+        return null;
+    }
+
+    @Override
+    public Booking addStatusBooking(Booking booking) {
+        return null;
+    }
+
+    @Override
+    public Booking getBookingById(long bookingId) {
+        return null;
+    }
+
+    @Override
+    public List<Booking> findAllBookingByUserId(String state, long userId) {
+        return null;
+    }
+
+    @Override
+    public List<Booking> findAllBookingByOwnerId(String state, long userId) {
+        return null;
+    }
 }
