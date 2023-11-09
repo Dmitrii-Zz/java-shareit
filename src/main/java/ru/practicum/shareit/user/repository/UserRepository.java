fd165ab4835d838fedbcd3f4929071ddd731a772
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-11-09 23:02:37 +0700

    fix: убрать версию у зависимости jpa

--- src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
@@ -1,21 +1,7 @@
 package ru.practicum.shareit.user.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.shareit.user.model.User;
 
-import java.util.List;
-
-public interface UserRepository {
-
-    User createUser(User user);
-
-    List<User> getAllUsers();
-
-    User updateUser(User user);
-
-    void deleteUser(long id);
-
-    User getUserById(long id);
-
-    boolean findUserByID(long id);
-
+public interface UserRepositoryH extends JpaRepository<User, Long> {
 }
--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
---INSERT INTO users (user_name, user_email) VALUES
---    ('Dimon', 'dimon2000@mail.ru');
