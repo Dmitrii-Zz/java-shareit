3e31031e676b4a44d8c1d0e38a144c3d40d50db0
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-11-11 19:31:28 +0700

    refactor: изменить имя колонки id в БД вещи

---
+++ src/main/java/ru/practicum/shareit/exceptions/ex/CheckStartAndEndBookingException.java
@@ -0,0 +1,2 @@
+package ru.practicum.shareit.exceptions.ex;public class CheckStartAndEndBookingException {
+}
--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -16,6 +16,7 @@ import javax.validation.constraints.NotNull;
 @NoArgsConstructor
 public class Item {
 
+    @Column(name = "item_id")
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
