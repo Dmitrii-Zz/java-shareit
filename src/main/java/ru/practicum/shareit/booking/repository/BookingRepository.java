ab96b22936115d6b1fde97bababe5350f57796bc
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-11-11 19:53:03 +0700

    feat: создать репозиторий для сущности аренда

--- src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
@@ -1,18 +1,8 @@
 package ru.practicum.shareit.booking.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.shareit.booking.model.Booking;
 
-import java.util.List;
+public interface BookingRepository extends JpaRepository<Booking, Long> {
 
-public interface BookingRepository {
-
-    Booking createBooking(Booking booking);
-
-    Booking addStatusBooking(Booking booking);
-
-    Booking getBookingById(long bookingId);
-
-    List<Booking> findAllBookingByUserId(String state, long userId);
-
-    List<Booking> findAllBookingByOwnerId(String state, long userId);
 }
