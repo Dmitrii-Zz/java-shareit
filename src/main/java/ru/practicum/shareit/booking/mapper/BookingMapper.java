9fee4fedc98bf5206fbc450c3d888dc6c81fccac
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-11-11 19:51:42 +0700

    feat: написать маппер для сущности аренда

--- src/main/java/ru/practicum/shareit/booking/mapper/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/mapper/BookingMapper.java
@@ -1,2 +1,31 @@
-package ru.practicum.shareit.booking.mapper;public class bookingMapper {
+package ru.practicum.shareit.booking.mapper;
+
+import org.springframework.stereotype.Component;
+import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.item.mapper.ItemMapper;
+import ru.practicum.shareit.user.mapper.UserMapper;
+
+@Component
+public class BookingMapper {
+
+    public static Booking toBooking(BookingDto bookingDto) {
+        return Booking.builder()
+                .start(bookingDto.getStart())
+                .end(bookingDto.getEnd())
+                .booker(UserMapper.toUser(bookingDto.getBooker()))
+                .item(ItemMapper.toItem(bookingDto.getItem()))
+                .status(bookingDto.getStatus())
+                .build();
+    }
+
+    public static BookingDto toBookingDto(Booking booking) {
+        return BookingDto.builder()
+                .start(booking.getStart())
+                .end(booking.getEnd())
+                .booker(UserMapper.toUserDto(booking.getBooker()))
+                .item(ItemMapper.toItemDto(booking.getItem()))
+                .status(booking.getStatus())
+                .build();
+    }
 }
