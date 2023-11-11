81cef11954f6712ef1a4c15e5e4a45feb1930fc6
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-11-11 19:41:32 +0700

    feat: добавить валидацию сущности аренда и описать её для БД

--- src/main/java/ru/practicum/shareit/booking/model/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/model/Booking.java
@@ -1,17 +1,45 @@
 package ru.practicum.shareit.booking.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.aspectj.lang.annotation.Before;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
+@Builder
+@Entity
+@Table(name = "bookings")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "start_date")
+    @NotNull(message = "Отсутствует начальная точка отсчета.")
     private LocalDateTime start;
+
+    @Column(name = "name_date")
+    @NotNull(message = "Отсутствует конечная точка отсчета.")
     private LocalDateTime end;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
     private Item item;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
     private User booker;
-    private BookingStatus bookingStatus;
+
+    @Column(name = "status")
+    @Enumerated
+    private BookingStatus status;
 }
