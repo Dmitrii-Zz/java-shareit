03b046db60bc7eb616864772442bd4022bd6b1e8
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-11-11 19:39:37 +0700

    feat: отловить исключеня валидации дат аренды и доступности вещи

--- src/main/java/ru/practicum/shareit/exceptions/controller/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exceptions/controller/ErrorHandler.java
@@ -4,9 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ru.practicum.shareit.exceptions.ex.CheckUserException;
-import ru.practicum.shareit.exceptions.ex.ItemNotFoundException;
-import ru.practicum.shareit.exceptions.ex.UserNotFoundException;
+import ru.practicum.shareit.exceptions.ex.*;
 import ru.practicum.shareit.exceptions.model.ErrorResponse;
 
 @RestControllerAdvice
@@ -27,6 +25,18 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleItemNotFoundException(final ItemNotFoundException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleCheckAvailableItemException(final CheckAvailableItemException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleCheckStartAndEndBookingException(final CheckStartAndEndBookingException e) {
         return new ErrorResponse(e.getMessage());
     }
 }
