923e28d80ad9b94bcc66ab2c07d2f8e955613e4f
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-11-23 20:48:44 +0700

    feat: написать репозиторий для запросов вещей

--- src/main/java/ru/practicum/shareit/request/repository/ItemRequestRepository.java
+++ src/main/java/ru/practicum/shareit/request/repository/ItemRequestRepository.java
@@ -1,2 +1,8 @@
-package ru.practicum.shareit.request.repository;public interface ItemRequestRepository {
+package ru.practicum.shareit.request.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.practicum.shareit.request.model.ItemRequest;
+
+public interface ItemRequestRepository extends JpaRepository<ItemRequest, Long> {
+
 }
