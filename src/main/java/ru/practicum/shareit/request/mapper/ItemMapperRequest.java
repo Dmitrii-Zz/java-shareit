59dc9ec53c9816ce31e9a712fca0663796930a0e
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-11-23 20:49:18 +0700

    feat: создать маппер для запросов вещей

--- src/main/java/ru/practicum/shareit/request/mapper/ItemMapperRequest.java
+++ src/main/java/ru/practicum/shareit/request/mapper/ItemMapperRequest.java
@@ -1,2 +1,9 @@
-package ru.practicum.shareit.request.mapper;public class ItemMapperRequest {
+package ru.practicum.shareit.request.mapper;
+
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+
+public class ItemMapperRequest {
+    public static ItemRequestDto toItemRequest (ItemRequestDto itemRequestDto) {
+        return null;
+    }
 }
