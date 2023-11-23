a23db5e94da96754a52d8d65785e681b91f27b9c
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-11-23 20:48:13 +0700

    feat: создать основные методы сервиса запросов

--- src/main/java/ru/practicum/shareit/request/service/ItemRequestService.java
+++ src/main/java/ru/practicum/shareit/request/service/ItemRequestService.java
@@ -1,2 +1,30 @@
-package ru.practicum.shareit.request.service;public class ItemService {
+package ru.practicum.shareit.request.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.request.repository.ItemRequestRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ItemRequestService {
+    private final ItemRequestRepository itemRequestStorage;
+
+    public ItemRequestDto addRequest(ItemRequestDto itemRequestDto) {
+        return null;
+    }
+
+    public List<ItemRequestDto> getAllItemRequest(long userId) {
+        return null;
+    }
+
+    public List<ItemRequestDto> getAllOtherUsersItemRequest() {
+        return null;
+    }
+
+    public ItemRequestDto getItemRequest(long requestId) {
+        return null;
+    }
 }
