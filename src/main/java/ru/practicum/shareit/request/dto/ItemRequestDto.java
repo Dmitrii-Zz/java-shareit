8eb7c1d387389c38e697e5b293a1fdda97e95a74
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-11-25 19:16:08 +0700

    feat: написать дто для запроса вещи

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,17 +1,22 @@
 package ru.practicum.shareit.request.dto;
 
+import lombok.Builder;
 import lombok.Data;
 import ru.practicum.shareit.user.model.User;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
-/**
- * TODO Sprint add-item-requests.
- */
 @Data
+@Builder
 public class ItemRequestDto {
+
     private long id;
+
+    @NotBlank(message = "В запросе отсутствует описание запроса")
     private String description;
+
     private User requestor;
+
     private LocalDateTime created;
 }
