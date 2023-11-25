17884a4a219c3420253e53cad06494b41c95f2f3
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-11-25 19:22:30 +0700

    feat: написать создание запроса вещи

--- src/main/java/ru/practicum/shareit/request/service/ItemRequestService.java
+++ src/main/java/ru/practicum/shareit/request/service/ItemRequestService.java
@@ -3,17 +3,27 @@ package ru.practicum.shareit.request.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.request.mapper.ItemRequestMapper;
+import ru.practicum.shareit.request.model.ItemRequest;
 import ru.practicum.shareit.request.repository.ItemRequestRepository;
+import ru.practicum.shareit.user.mapper.UserMapper;
+import ru.practicum.shareit.user.service.UserService;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ItemRequestService {
     private final ItemRequestRepository itemRequestStorage;
+    private final UserService userService;
 
-    public ItemRequestDto addRequest(ItemRequestDto itemRequestDto) {
-        return null;
+    public ItemRequestDto addRequest(ItemRequestDto itemRequestDto, long userId) {
+        userService.checkExistsUser(userId);
+        ItemRequest itemRequest = ItemRequestMapper.toItemRequest(itemRequestDto);
+        itemRequest.setRequestor(UserMapper.toUser(userService.getUserById(userId)));
+        itemRequest.setCreated(LocalDateTime.now());
+        return ItemRequestMapper.toItemRequestDto(itemRequestStorage.save(itemRequest));
     }
 
     public List<ItemRequestDto> getAllItemRequest(long userId) {
