9468ad333c7afdca56995ab399ade6babd887a3b
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-12-03 22:27:59 +0700

    test: тесты json

---
+++ src/test/java/ru/practicum/shareit/user/UserJsonTest.java
@@ -0,0 +1,31 @@
+package ru.practicum.shareit.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+import ru.practicum.shareit.user.dto.UserDto;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+@JsonTest
+public class UserJsonTest {
+    @Autowired
+    private JacksonTester<UserDto> json;
+
+    @Test
+    void testUserDto() throws Exception {
+        UserDto userDto = UserDto.builder()
+                .id(1)
+                .name("Dmitrij")
+                .email("Dimon@mail.ru")
+                .build();
+
+        JsonContent<UserDto> result = json.write(userDto);
+
+        assertThat(result).extractingJsonPathNumberValue("$.id").isEqualTo(1);
+        assertThat(result).extractingJsonPathStringValue("$.name").isEqualTo("Dmitrij");
+        assertThat(result).extractingJsonPathStringValue("$.email").isEqualTo("Dimon@mail.ru");
+    }
+}
