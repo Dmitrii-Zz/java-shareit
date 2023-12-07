6550f80d70fc0826cbb62e86f9aeff0b97fa061b
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2023-12-07 19:06:25 +0700

    feat: перенести код DTO в gateway

--- gateway/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,2 +1,30 @@
-package ru.practicum.shareit.item.dto;public class ItemDto {
+package ru.practicum.shareit.item.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import ru.practicum.shareit.booking.dto.BookingOwnerDto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+public class ItemDto {
+    private long id;
+
+    @NotBlank(message = "В запросе отсутствует имя вещи.")
+    private String name;
+
+    @NotBlank(message = "В запросе отсутствует описание вещи.")
+    private String description;
+
+    @NotNull(message = "В запросе отсутствует статус запроса к аренде.")
+    private Boolean available;
+
+    private BookingOwnerDto lastBooking;
+
+    private BookingOwnerDto nextBooking;
+
+    private List<CommentDto> comments;
+
+    @JsonProperty("requestId")
+    private Long itemRequest;
 }
